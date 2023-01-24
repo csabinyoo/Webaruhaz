734b04d91f6b03cf24fe4e7d6b6116a67a170a4c
csabinyoo <ntoria8@citromail.hu>
2023-01-24 10:23:40 +0100

    Add

--- index.js
+++ index.js
@@ -7,8 +7,26 @@ const dataFile = "./data/products.json"
 
 app.get('/products', function (req, res) {
   fs.readFile(dataFile, (error, data) => {
-    let products = data;
-    res.end(products)
+    let products = JSON.parse(data);
+    res.send(products)
+  })
+})
+
+app.get('/products/:id', function (req, res) {
+  const id = req.params.id;
+  console.log(id);
+  fs.readFile(dataFile, (error, data) => {
+    let products = JSON.parse(data);
+    const productById = products.find(product => product.id === id);
+    if (!productById) {
+      let message = {
+        error: `Not found id: ${id}`
+      };
+      res.status(404);
+      res.send(message);
+      return;
+    }
+    res.send(productById)
   })
 })
 
