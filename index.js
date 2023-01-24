00f64b37f5fca01645d9abec6b55107fb6194f4a
csabinyoo <ntoria8@citromail.hu>
2023-01-24 10:59:24 +0100

    Add

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ const express = require('express')
 const app = express()
 let port = 3000
 const fs = require('fs');
+const uniqid = require('uniqid');
 
 const dataFile = "./data/products.json"
+
+// Middleware
+app.use(express.json());
 
 app.get('/products', function (req, res) {
   fs.readFile(dataFile, (error, data) => {
     let products = JSON.parse(data);
     res.send(products)
   })
-})
+});
 
 app.get('/products/:id', function (req, res) {
   const id = req.params.id;
@@ -29,6 +33,29 @@ app.get('/products/:id', function (req, res) {
     res.send(productById)
   })
 })
+
+app.post('/products', function (req, res) {
+  const body = req.body;
+
+  const newProduct = {
+    id: uniqid(),
+    name: body.name,
+    quantity: body.quantity,
+    price: body.price,
+    type: body.type
+  }
+
+  fs.readFile(dataFile, (error, data) => {
+    // json -> objektumlista
+    let products = JSON.parse(data);
+    products.push(newProduct);
+    // objektumlista -> json
+    products = JSON.stringify(products);
+    fs.writeFile(dataFile, products, (error) => {
+      res.send(newProduct);
+    });
+  });
+});
 
 app.listen(port, ()=> {
   console.log(`Express server ok. port: ${port}`);
