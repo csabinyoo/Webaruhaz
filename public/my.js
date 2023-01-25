23fb7838036e9a2e9e72a79e8ce6e3e5af46b7b1
csabinyoo <ntoria8@citromail.hu>
2023-01-25 09:43:19 +0100

    Add

--- public/my.js
+++ public/my.js
@@ -1,6 +1,26 @@
-console.log();
 const url = "http://localhost:3000/products/"
 
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+async function getProducts(){
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+};
+getProducts();
+
+async function postProduct(){
+    let body = {
+        name: "Á66",
+        quantity: 24,
+        price: 6500,
+        type: "fémáru"
+    }
+    let response = await fetch (url, {
+        Method: 'POST',
+        Headers: {
+            Accept: 'application.json',
+            'Content-Type': 'appliaction/json'
+        },
+        Body: body,
+        Cache: 'default'
+    })
+};
